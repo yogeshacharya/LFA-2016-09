705a702b79d2edb61f614205de628e8378e4e990
Yogesh <Yogesh@YOGESH-PC>
2016-08-16 07:20:58 +0545

    soln

--- ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d08b97ea-eca2-4982-a7aa-41ecb423b2fe")]
+[assembly: Guid("78795e1f-27e8-46d6-ac5b-964eb1538008")]
 
 // Version information for an assembly consists of the following four values:
 //
